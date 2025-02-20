8e33e3eebd030f2a1ca790e756b734e16b74b802
TheSeagullDev <67653706+TheSeagullDev@users.noreply.github.com>
2025-02-20 15:32:37 -0500

    refactor to use class declarations

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 const myLibrary = [];
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
+    toggleRead() {
+        this.read = !this.read;
+    }
 }
 
-Book.prototype.toggleRead = function() {
-    this.read = !this.read;
-}
 
 function addBookToLibrary(title, author, pages, read) {
     myLibrary.push(new Book(title, author, pages, read));
